4594c4888640afaa197f899622e57e734a4d9ff2
Carlos M. Martinez <carlos@lacnic.net>
2019-10-25 16:38:16 -0300

    struct reorg, add exporter

--- dns_test.py
+++ dns_test.py
@@ -4,6 +4,7 @@
 
 import dns.resolver
 import pprint
+import click
 
 resolver = dns.resolver.Resolver()
 
@@ -52,17 +53,27 @@ def get_serials_for_zone(w_master, w_zone):
 
 ## end def #####################################################################
 
+@click.command()
+@click.option('--zone', default=None, help='Zone to check.')
+@click.option('--master', default=None, help='Master NS server (to get NSSet).')
+def checkserial(zone, master):
+    print("zone {}, master {}".format(zone,master))
+    sr = get_serials_for_zone(master, zone)
+    pp = pprint.PrettyPrinter(width=70)
+    pp.pprint(sr)
+
 ## BEGIN MAIN ##################################################################
 if __name__ == "__main__":
 
-    serials_result = get_serials_for_zone('200.3.13.11', '179.in-addr.arpa')
+    # serials_result = get_serials_for_zone('200.3.13.11', '179.in-addr.arpa')
+    checkserial()
 
     # ans = dns.resolver.query('lacnic.net', 'SOA')
     # for rdata in ans: 
     #     print(rdata.serial)
 
-    pp = pprint.PrettyPrinter()
-    pp.pprint(serials_result)
+    # pp = pprint.PrettyPrinter()
+    # pp.pprint(serials_result)
 
     # print(repr(serials_result))
 
